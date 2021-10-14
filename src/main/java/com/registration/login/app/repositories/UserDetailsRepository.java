88fa6aa5e1a8ed06feb6d5366e7414679961ae7f
Aditya Khadatkar <apk26012000@gmail.com>
2021-10-14 12:00:18 +0530

    resolved issue regarding SQL query caused while using H2 database

--- src/main/java/com/registration/login/app/repositories/UserDetailsRepository.java
+++ src/main/java/com/registration/login/app/repositories/UserDetailsRepository.java
@@ -10,22 +10,22 @@ import com.registration.login.app.entities.UserDetails;
 
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Integer> {
 
-	@Query(value = "select * from user_details u where binary u.username = :username", nativeQuery = true)
+	@Query(value = "select * from user_details u where  u.username = :username", nativeQuery = true)
 	public UserDetails findByUsername(String username);
 
-	@Query(value = "select * from user_details u where binary u.username= :username AND u.password = :password", nativeQuery = true)
+	@Query(value = "select * from user_details u where  u.username= :username AND u.password = :password", nativeQuery = true)
 	public UserDetails findByPassword(String password, String username);
 
 	@Modifying(clearAutomatically = true, flushAutomatically = true)
 	@Transactional
-	@Query(value = "update user_details SET failed_tries = :failedTries where binary username = :username ", nativeQuery = true)
+	@Query(value = "update user_details SET failed_tries = :failedTries where username = :username ", nativeQuery = true)
 	public void updateUserFailedTries(String username, Integer failedTries);
 
 	@Modifying(clearAutomatically = true, flushAutomatically = true)
 	@Transactional
-	@Query(value = "update user_details SET locked = :isLocked where binary username = :username ", nativeQuery = true)
+	@Query(value = "update user_details SET locked = :isLocked where  username = :username ", nativeQuery = true)
 	public void updateIsLocked(String username, Integer isLocked);
 
-	@Query(value = "select * from user_details where binary username = :username", nativeQuery = true)
+	@Query(value = "select * from user_details where  username = :username", nativeQuery = true)
 	public Integer existsUserDetailsByUsername(String username);
 }
